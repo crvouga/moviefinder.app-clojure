4dd8ad4cc3afa23bbd319290913734fabadf55e8
Chris Vouga <crvouga@gmail.com>
2024-05-31 15:16:16 -0700

    chore: Add hiccup dependency and update handler to use hiccup for generating HTML

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [hiccup "2.0.0-RC3"]
                  [ring/ring-core "1.9.3"]
                  [ring/ring-jetty-adapter "1.9.3"]
                  [ring/ring-devel "1.9.3"]] ; Added ring-devel for hot reloading
--- src/app/core.clj
+++ src/app/core.clj
@@ -1,14 +1,34 @@
 (ns app.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [hiccup2.core]))
 
-(defn html []
+(defn html [html]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "<h1>Hello, World !!!!!</h1>"})
+   :body (str (hiccup2.core/html html))})
 
-(defn handler [request]
-  (html))
+(defn button 
+  [props & children]
+  [:button.bg-blue-500.hover:bg-blue-700.text-white.font-bold.py-2.px-4.rounded.active:opacity-50 props children])
+
+
+(defn ring-request->request [ring-request]
+  {:name (:uri ring-request)})
+
+(defn handler [ring-request]
+  (let [request (ring-request->request ring-request)]
+    (print request))
+  
+  (html
+   [:html
+    [:head
+     [:title "COOL"]
+     [:script {:src "https://cdn.tailwindcss.com"}]
+     [:script {:src "https://unpkg.com/htmx.org@1.9.12"}]]
+    [:body.bg-neutral-950.text-white {:hx-boost true}
+     [:h1 "COOL"] 
+     (button {:hx-get ::clicked-append :hx-swap :afterend} "Click me")]]))
 
 (defn -main []
   (run-jetty (wrap-reload #'handler) {:port 3000 :join? false}))
