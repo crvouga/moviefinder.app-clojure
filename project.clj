523417cc44b06a72ade7fcb2170c169fb45690f4
Chris Vouga <crvouga@gmail.com>
2024-05-31 14:56:36 -0700

    chore: Add hot reloading to Clojure web server

--- project.clj
+++ project.clj
@@ -1,7 +1,13 @@
 (defproject app "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A simple Clojure web server with hot reloading"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core "1.9.3"]
+                 [ring/ring-jetty-adapter "1.9.3"]
+                 [ring/ring-devel "1.9.3"]] ; Added ring-devel for hot reloading
+  :main ^:skip-aot app.core
+  :target-path "target/%s"
+  :profiles {:dev {:dependencies [[ring/ring-mock "0.4.0"]]}}
   :repl-options {:init-ns app.core})
--- src/app/core.clj
+++ src/app/core.clj
@@ -1,6 +1,14 @@
-(ns app.core)
+(ns app.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn html []
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "<h1>Hello, World !!!!!</h1>"})
+
+(defn handler [request]
+  (html))
+
+(defn -main []
+  (run-jetty (wrap-reload #'handler) {:port 3000 :join? false}))
