89ce88d0bfb02eef44c78a3632b85a02933d4df8
Chris Vouga <crvouga@gmail.com>
2024-06-01 01:59:34 -0700

    refactor: Implement lazy loading for movie posters and YouTube videos in feed.clj

--- src/app/feed.clj
+++ src/app/feed.clj
@@ -17,13 +17,26 @@
                            :allowFullScreen true}])
    [:p (-> movie :movie/title)]])
 
+(defn view-swiper-slide [children]
+  [:swiper-slide.w-full.h-full.overflow-hidden.max-h-full
+   children])
+
+(defn view-swiper-container [children]
+  [:swiper-container.w-full.flex-1.max-h-full.overflow-hidden {:slides-per-view 1 :direction :vertical}
+   children])
+
+(defn view-swiper [view-slides]
+  (view-swiper-container
+   (for [vide-slide view-slides]
+     (view-swiper-slide vide-slide))))
+
+
 (defn view-feed-panel []
   (let [movies (app.movie.db.core/find-movies movie-db {})]
     [:div.w-full.max-h-full.overflow-hidden.h-full.flex.flex-col
-     [:swiper-container.w-full.flex-1.max-h-96 {:slides-per-view 1 :direction :vertical}
+     (view-swiper
       (for [movie (-> movies :results)]
-        [:swiper-slide.w-full.h-full.overflow-hidden.max-h-full
-         (view-feed-item movie)])]]))
+         (view-feed-item movie)))]))
 
 (defn view-feed-route []
   (app.routes/view-app-tabs-layout app.routes/route-feed (view-feed-panel)))
