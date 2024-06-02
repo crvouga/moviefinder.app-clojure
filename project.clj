84a834c6b96db2c69feb20693e9e3c76e0230014
Chris Vouga <crvouga@gmail.com>
2024-06-02 16:54:26 -0700

    refactor: Update project.clj with new dependencies for Clojure and JDBC

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/java.jdbc "0.7.8"]
+                 [org.xerial/sqlite-jdbc "3.23.1"]
                  [hiccup "2.0.0-RC3"]
                  [ring/ring-core "1.9.3"]
                  [ring/ring-jetty-adapter "1.9.3"]
---
+++ src/moviefinder/app/db.clj
@@ -0,0 +1,18 @@
+(ns moviefinder.app.db
+  (:require [clojure.java.jdbc]))
+
+(def db
+  {:classname   "org.sqlite.JDBC"
+   :subprotocol "sqlite"
+   :subname     "db/database.db"})
+
+(defn print-result-set
+  "prints the result set in tabular form"
+  [result-set]
+  (doseq [row result-set]
+    (println row)))
+
+(defn query
+  "execute query and return lazy sequence"
+  [sql-query]
+  (clojure.java.jdbc/query db [sql-query]))
