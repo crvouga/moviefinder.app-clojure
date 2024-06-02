a7b0b4a237e2dc16eeab9c48fe90b32942f601df
Chris Vouga <crvouga@gmail.com>
2024-06-02 13:10:51 -0700

    refactor: Update movie details view to lazy load images and improve performance

--- src/moviefinder/app/home.clj
+++ src/moviefinder/app/home.clj
@@ -23,7 +23,7 @@
     {:href (movie-details-href movie)}
     (-> movie :movie/title)]])
 
-(defn view-feed-item [movie]
+(defn view-feed-slide [movie]
   [:div.w-full.flex.flex-col.justify-center.items-center.relative.h-full
    [:img.w-full.h-full.absolute.inset-0.-z-10.object-cover.bg-netural-200 {:src (-> movie :movie/poster-url) :loading :lazy}]
    [:a.w-full.flex-1.flex-col.justify-center.items-center.flex
@@ -37,25 +37,28 @@
         (view-youtube-video {:src youtube-video-url}))]
    #_(view-feed-item-title movie)])
 
-(defn view-swiper-slide [children]
-  [:swiper-slide.w-full.h-full.overflow-hidden.max-h-full
-   children])
-
 ;; https://swiperjs.com/element
-(defn view-swiper-container [props children]
-  [:swiper-container.w-full.flex-1.max-h-full.overflow-hidden (merge {:slides-per-view 1 :direction :vertical} props)
-   children])
 
-(defn view-feed [input]
+(defn view-feed-slides! [input]
   (let [movies (moviefinder.app.movie.db/find! movie-db {})]
-    [:div.w-full.max-h-full.overflow-hidden.h-full.flex.flex-col
-     (view-swiper-container {:initial-slide (-> input :request/route :feed/slide-index)}
-      (for [movie (-> movies :paginated/results)]
-        (view-swiper-slide
-          (view-feed-item movie))))]))
+    (for [[slide-index movie] (map-indexed vector (-> movies :paginated/results))]
+      [:swiper-slide.w-full.h-full.overflow-hidden.max-h-full
+       {:hx-post (moviefinder.app.route/encode {:route/name :noop})
+        :hx-target "none"
+        :hx-trigger "slideChange"
+        :hx-push-url (-> input :request/route (assoc :feed/slide-index slide-index) moviefinder.app.route/encode)}
+       (view-feed-slide movie)])))
+
+(defn view-feed! [input]
+  [:div.w-full.max-h-full.overflow-hidden.h-full.flex.flex-col
+   [:swiper-container.w-full.flex-1.max-h-full.overflow-hidden
+    {:slides-per-view 1
+     :direction :vertical
+     :initial-slide (-> input :request/route :feed/slide-index)}
+    (view-feed-slides! input)]])
 
 (defn view-home [input]
-  (moviefinder.app.view/view-app-tabs-layout {:route/name :home/home}  (view-feed input)))
+  (moviefinder.app.view/view-app-tabs-layout {:route/name :home/home}  (view-feed! input)))
 
 (defmethod moviefinder.app.requests/route-hx :noop [_request]
   {:status 200})
