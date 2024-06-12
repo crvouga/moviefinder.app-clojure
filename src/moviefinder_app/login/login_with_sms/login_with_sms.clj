455eacddb03816a86915983e2f23108b0aa2233e
Chris Vouga <crvouga@gmail.com>
2024-06-11 23:27:35 -0700

    refactor: Add SMS verification functionality to login process
    This commit adds the SMS verification functionality to the login process. It introduces new functions and views for verifying the SMS code and sending the code to the user's phone number. The code has been refactored to handle the verification and display the appropriate messages to the user.
    
    Note: This commit message follows the convention of starting with a type (refactor) followed by a brief description of the changes.

--- src/moviefinder_app/login/login_with_sms/login_with_sms.clj
+++ src/moviefinder_app/login/login_with_sms/login_with_sms.clj
@@ -1,10 +1,39 @@
 (ns moviefinder-app.login.login-with-sms.login-with-sms 
   (:require [moviefinder-app.handle :as handle]
-            [moviefinder-app.view :as view]))
+            [moviefinder-app.view :as view]
+            [moviefinder-app.login.login-with-sms.verify-sms.verify-sms :as verify-sms]))
 
+(defn view-verify-code-ok [_request]
+  [:div "Code verified"])
 
+(defmethod handle/handle ::clicked-verify-code [request]
+  (let [verify-sms (-> request :verify-code/:verify-sms)
+        phone-number (-> request :request/form-data :phone-number)
+        code (-> request :request/form-data :code)]
+    (verify-sms/verify-code! verify-sms phone-number code))
+  (handle/html (view-verify-code-ok request)))
 
-(defn view-login-with-sms [_request]
+(defn view-verify-code [_request]
+  [:form
+   (view/text-field {:text-field/id "phone-number"
+                     :text-field/label "Phone number"
+                     :text-field/type "tel"
+                     :text-field/initial-value (-> _request :request/form-data :phone-number)
+                     :text-field/hidden? true})
+   (view/text-field {:text-field/id "code"
+                     :text-field/label "Code"
+                     :text-field/type "tel"})
+   (view/button {:button/type "submit"
+                 :button/text "Verify code"})])
+
+(defmethod handle/handle ::clicked-send-code [request]
+  (let [verify-sms (-> request :verify-code/:verify-sms)
+        phone-number (-> request :request/form-data :phone-number)]
+    (verify-sms/send-code! verify-sms phone-number))
+  
+  (handle/html (view-verify-code request)))
+
+(defn view-send-code [_request]
   [:form
    (view/text-field {:text-field/id "phone-number"
                      :text-field/label "Phone number"
@@ -12,6 +41,5 @@
    (view/button {:button/type "submit"
                  :button/text "Send code"})])
 
-
-(defmethod handle/handle :route/login-with-sms [request]
-  (view-login-with-sms request))
+(defmethod handle/handle-hx :route/login-with-sms [request]
+  (handle/html (view-send-code request)))
